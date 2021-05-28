424196c9d4dd73c7e92bacb438807e1bd045f3b6
Bernese-Corgi <jnyng0621@gmail.com>
2021-05-28 22:06:30 +0900

    Tech interview

---
+++ Tech Interview/test.js
@@ -0,0 +1,13 @@
+var x;
+const y = null;
+
+console.log(typeof x); // undefined
+console.log(typeof y); // object
+
+console.log(y === null); // true
+console.log(y === undefined); // false
+
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+// console.log(null === undefined);
